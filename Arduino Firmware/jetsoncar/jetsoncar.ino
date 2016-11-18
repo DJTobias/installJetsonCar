bed0d87e0ecdca479c23815c5c6888afe8cecae3
Daniel Tobias <danielt728@gmail.com>
2016-11-18 11:16:20 -0500

    Working Code

--- Arduino Firmware/jetsoncar/jetsoncar.ino
+++ Arduino Firmware/jetsoncar/jetsoncar.ino
@@ -5,13 +5,18 @@
   JetsonHacks (2016)
 */
 
+/*
+Modified to use with Teensy 3.2
+
+*/
+
 #if defined(ARDUINO) && ARDUINO >= 100
-  #include "Arduino.h"
+#include "Arduino.h"
 #else
-  #include <WProgram.h>
+#include <WProgram.h>
 #endif
 
-#include <Servo.h> 
+#include <Servo.h>
 #define USB_USBCON
 #include <ros.h>
 #include <std_msgs/UInt16.h>
@@ -23,16 +28,16 @@
 ros::NodeHandle  nodeHandle;
 // These are general bounds for the steering servo and the
 // TRAXXAS Electronic Speed Controller (ESC)
-const int minSteering = 30 ;
-const int maxSteering = 150 ;
+const int minSteering = 1200 ;
+const int maxSteering = 1800 ;
 const int minThrottle = 0 ;
-const int maxThrottle = 150 ;
+const int maxThrottle = 1680 ;
 
 Servo steeringServo;
 Servo electronicSpeedController ;  // The ESC on the TRAXXAS works like a Servo
 
 std_msgs::Int32 str_msg;
-ros::Publisher chatter("chatter", &str_msg); 
+ros::Publisher chatter("chatter", &str_msg);
 
 // Arduino 'map' funtion for floating point
 double fmap (double toMap, double in_min, double in_max, double out_min, double out_max) {
@@ -41,46 +46,58 @@ double fmap (double toMap, double in_min, double in_max, double out_min, double 
 
 void driveCallback ( const geometry_msgs::Twist&  twistMsg )
 {
-  
+
   int steeringAngle = fmap(twistMsg.angular.z, 0.0, 1.0, minSteering, maxSteering) ;
   // The following could be useful for debugging
-  // str_msg.data= steeringAngle ;
-  // chatter.publish(&str_msg);
+   str_msg.data= steeringAngle ;
+   chatter.publish(&str_msg);
   // Check to make sure steeringAngle is within car range
-  if (steeringAngle < minSteering) { 
+  if (steeringAngle < minSteering) {
     steeringAngle = minSteering;
   }
   if (steeringAngle > maxSteering) {
     steeringAngle = maxSteering ;
   }
-  steeringServo.write(steeringAngle) ;
-  
+  steeringServo.writeMicroseconds(steeringAngle) ;
+  if(steeringAngle < 1450)
+    digitalWrite(16, HIGH - digitalRead(16));
+  else if(steeringAngle> 1550)
+    digitalWrite(15, HIGH - digitalRead(15));
+  else {
+    digitalWrite(15,LOW);
+    digitalWrite(16,LOW);
+  }
+
   // ESC forward is between 0.5 and 1.0
   int escCommand ;
   if (twistMsg.linear.x >= 0.5) {
-    escCommand = (int)fmap(twistMsg.linear.x, 0.5, 1.0, 90.0, maxThrottle) ;
+    escCommand = (int)fmap(twistMsg.linear.x, 0.5, 1.0, 1500.0, maxThrottle) ;
   } else {
-    escCommand = (int)fmap(twistMsg.linear.x, 0.0, 1.0, 0.0, 180.0) ;
+    //escCommand = (int)fmap(twistMsg.linear.x, 0.0, 1.0, 0.0, 1780.0) ;
+    escCommand = (int)fmap(twistMsg.linear.x, 0.0, 0.5, 0.0, 1500.0) ;
   }
   // Check to make sure throttle command is within bounds
-  if (escCommand < minThrottle) { 
+  if (escCommand < minThrottle) {
     escCommand = minThrottle;
   }
   if (escCommand > maxThrottle) {
     escCommand = maxThrottle ;
   }
   // The following could be useful for debugging
-  // str_msg.data= escCommand ;
-  // chatter.publish(&str_msg);
-  
-  electronicSpeedController.write(escCommand) ;
-  digitalWrite(13, HIGH-digitalRead(13));  //toggle led  
- 
+   str_msg.data= escCommand ;
+   chatter.publish(&str_msg);
+
+  electronicSpeedController.writeMicroseconds(escCommand) ;
+  digitalWrite(13, HIGH - digitalRead(13)); //toggle led
+
 }
 
 ros::Subscriber<geometry_msgs::Twist> driveSubscriber("/jetsoncar_teleop_joystick/cmd_vel", &driveCallback) ;
 
-void setup(){
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(16, OUTPUT);
+  pinMode(15, OUTPUT);
   pinMode(13, OUTPUT);
   Serial.begin(115200) ;
   nodeHandle.initNode();
@@ -93,13 +110,35 @@ void setup(){
   electronicSpeedController.attach(10); // ESC is on pin 10
   // Initialize Steering and ESC setting
   // Steering centered is 90, throttle at neutral is 90
-  steeringServo.write(90) ;
-  electronicSpeedController.write(90) ;
-  delay(1000) ;
+  steeringServo.writeMicroseconds(1500) ;
+  //delay(10000);                                           //ESC Activation sequence
+  //digitalWrite(13, HIGH - digitalRead(13));
+  //electronicSpeedController.writeMicroseconds(1700);
+  //delay(1000);
+  //electronicSpeedController.writeMicroseconds(1300);
+  //delay(1000);
+  //electronicSpeedController.writeMicroseconds(1500);
+  //delay(1000);
   
+
 }
 
-void loop(){
-  nodeHandle.spinOnce();
+void loop() {
+  nodeHandle.spinOnce();        //refreshes nodes
   delay(1);
+    if(digitalRead(2)){  //future kill command between RF Receiver and Pin 2 of Teensy
+    delay(1);
+    if(digitalRead(2)){
+      for(int i=0;i<20;i++){
+      digitalWrite(15,HIGH);
+      digitalWrite(16,HIGH);
+      delay(200);
+      nodeHandle.spinOnce();
+      digitalWrite(15,LOW);
+      digitalWrite(16,LOW);
+      delay(200);
+      nodeHandle.spinOnce(); //
+      }
+    }
+  }
 }
